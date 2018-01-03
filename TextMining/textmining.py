22e4f94a482616cb857975b604cfcb0542e80839
sangho61 <sanghoeum@gmail.com>
2018-01-03 10:54:14 -0500

    parsed all posts

--- TextMining/textmining.py
+++ TextMining/textmining.py
@@ -3,26 +3,29 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = 'https://www.data.go.kr/information/qna/index.do?currentPage=1'
-# html = urllib.request.urlopen(url)
-r = requests.get(url)
-print(r.encoding)
-soupdata = BeautifulSoup(r.content, 'html.parser')
-tbody=soupdata.find('tbody')
-
-t1 = list(tbody.stripped_strings)
 m1 ={'No':[],'Title':[],'Writer':[], 'Date':[], 'Status':[]}
-for string in range(len(t1)):
-    if string % 5 == 0:
-        m1['No'].append(t1[string])
-    elif string % 5 ==1:
-        m1['Title'].append(t1[string])
-    elif string % 5 == 2:
-        m1['Writer'].append(t1[string])
-    elif string % 5 == 3:
-        m1['Date'].append(t1[string])
-    else:
-        m1['Status'].append(t1[string])
+for i in range(10):
+    url = 'https://www.data.go.kr/information/qna/index.do?currentPage='
+    # html = urllib.request.urlopen(url)
+    n= str(i+1)
+    new_url=url+n
+    r = requests.get(new_url)
+    soupdata = BeautifulSoup(r.content, 'html.parser')
+    tbody=soupdata.find('tbody')
+
+    t1 = list(tbody.stripped_strings)
+
+    for string in range(len(t1)):
+        if string % 5 == 0:
+            m1['No'].append(t1[string])
+        elif string % 5 ==1:
+            m1['Title'].append(t1[string])
+        elif string % 5 == 2:
+            m1['Writer'].append(t1[string])
+        elif string % 5 == 3:
+            m1['Date'].append(t1[string])
+        else:
+            m1['Status'].append(t1[string])
 
 
 def write_csv(text, file_name ="mining.csv"):
