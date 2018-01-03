e02d23c7884440853a4d2ac10851758c2598f576
sangho61 <sanghoeum@gmail.com>
2018-01-03 02:27:24 -0500

    finish lab05

--- BasicPractice/deeplearningzerotoall/lab05.py
+++ BasicPractice/deeplearningzerotoall/lab05.py
@@ -8,7 +8,7 @@ X = tf.placeholder(tf.float32, shape=[None, 2])
 Y = tf.placeholder(tf.float32, shape=[None, 1])
 
 W = tf.Variable(tf.random_normal([2,1]), name = 'weight')
-b = tf.Variavle(tf.random_normal([1]), name = 'bias')
+b = tf.Variable(tf.random_normal([1]), name = 'bias')
 
 # Hypothesis using sigmoid
 hypothesis = tf.sigmoid(tf.matmul(X,W)+b)
@@ -22,3 +22,14 @@ train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
 predicted = tf.cast(hypothesis > 0.5, dtype = tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype = tf.float32))
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
+        if step % 200 == 0:
+            print(step, cost_val)
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict = {X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy:  ", a)
